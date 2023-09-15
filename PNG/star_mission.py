0bb39b80cc44929124ed35443dd09fe97d96b900
Noesu <138776947+Noesu@users.noreply.github.com>
2023-09-15 19:58:07 +0530

    Rewrited methods of collision check for every object Added strength value for player and asteroid Added upgrade and asteroid crash sound
    To do:
    Check missle collide with other missle
    Add flashing visualisation of score received
    Add permanent visualisation of player defence
    Add defence type upgrade
    Add player weapon types
    Add score sound?
    Add menu
    Add hiscores saving

--- PNG/star_mission.py
+++ PNG/star_mission.py
@@ -29,7 +29,7 @@ class Wind(games.Sprite):
     def __init__(self, wind_type):
         super(Wind, self).__init__(image=Wind.images[wind_type],
                                    y=0,
-                                   x=random.randint(0, 639),
+                                   x=random.randint(0, 479),
                                    dy=random.choice([1, 2, 3]),
                                    is_collideable=False)
 
@@ -49,10 +49,13 @@ class Asteroid(games.Sprite):
               TYPE2: games.load_image("asteroid2.png"),
               TYPE3: games.load_image("asteroid3.png"),
               TYPE4: games.load_image("asteroid4.png")}
+    sound = games.load_sound("ast_crash1.wav")
+    strength_value = 20
+    object_upgrade = 0
 
     def __init__(self, ast_type):
         super(Asteroid, self).__init__(image=Asteroid.images[ast_type],
-                                       x=random.randint(0, 639),
+                                       x=random.randint(0, 479),
                                        y=0,
                                        dy=random.randint(1, 3),
                                        is_collideable=True)
@@ -60,20 +63,36 @@ class Asteroid(games.Sprite):
     def update(self):
         if self.top >= games.screen.height:
             self.destroy()
+        self.check_collision()
 
-    def handle_hit(self):
-        self.destroy()
-        GameObject.score.value += 10
+    def check_collision(self):
+        for space_object in self.overlapping_sprites:
+            collision_confirmed = space_object.handle_collision(self.strength_value)
+            if collision_confirmed:
+                self.sound.play()
+                self.destroy()
 
-    def receive_upgrade(self):
-        return
+    @staticmethod
+    def receive_upgrade(*upgrade_value):
+        return False
+
+    def handle_missle_hit(self, missle_power):
+        self.strength_value -= missle_power
+        if self.strength_value <= 0:
+            self.destroy()
+        return True
+
+    def handle_collision(self, strength_value):
+        return False
 
 
 class Upgrade(games.Sprite):
     image = games.load_image("missle1.png", transparent=True)
+    sound = games.load_sound("missle_speed_upgrade.mp3")
+    upgrade_value = 1
 
     def __init__(self):
-        x_pos = random.randint(0, 639)
+        x_pos = random.randint(0, 479)
         y_speed = 1
         super(Upgrade, self).__init__(image=Upgrade.image,
                                       x=x_pos,
@@ -86,77 +105,94 @@ class Upgrade(games.Sprite):
             self.destroy()
         self.check_upgrade()
 
-    def handle_hit(self):
-        return
-
     def check_upgrade(self):
-        for upgrade_object in self.overlapping_sprites:
-            upgrade_object.receive_upgrade()
-            self.destroy()
+        for space_object in self.overlapping_sprites:
+            upgrade_received = space_object.receive_upgrade(self.upgrade_value)
+            if upgrade_received:
+                self.sound.play()
+                self.destroy()
+
+    def handle_collision(self, *strength):
+        return False
+
+    @staticmethod
+    def handle_missle_hit(*missle_power):
+        return False
 
 
 class Player(games.Sprite):
     image = games.load_image("spaceShips_001.png", transparent=True)
+    defence = 60
+    object_upgrade = 0
 
     def __init__(self):
         super(Player, self).__init__(image=Player.image,
-                                     x=games.screen.width/2,
-                                     y=games.screen.height/2,
+                                     x=games.screen.width / 2,
+                                     y=games.screen.height / 2,
                                      is_collideable=True)
         self.reload_timer = 0
         self.time_to_spawn_asteroid = 50
         self.last_upgrade = 0
 
     def update(self):
-        """ Move to mouse x position. """
+        # Spaceship orientation
         self.x = games.mouse.x
         self.y = games.mouse.y
-
         if self.reload_timer > 0:
             self.reload_timer -= 1
-
         if self.x < 0:
             self.x = 0
-
         if self.x > games.screen.width:
             self.x = games.screen.width
 
+        # Weapon control
         if games.mouse.is_pressed(0):
-            self.shoot()
+            self.launch_missle()
+            # print(GameObject.score.value)
 
+        # Asteroid generator
         self.time_to_spawn_asteroid -= 1
-
         if self.time_to_spawn_asteroid == 0:
             self.time_to_spawn_asteroid = 50
             asteroid_type = random.choice([Asteroid.TYPE1, Asteroid.TYPE2, Asteroid.TYPE3, Asteroid.TYPE4])
             new_asteroid = Asteroid(ast_type=asteroid_type)
             games.screen.add(new_asteroid)
 
+        # Upgrade generator
         if GameObject.score.value % 100 == 0 and GameObject.score.value != self.last_upgrade:
             upgrade_object = Upgrade()
             self.last_upgrade = GameObject.score.value
             games.screen.add(upgrade_object)
-            GameObject.score.value += 50
+            # GameObject.score.value += 50
 
-    def shoot(self):
+    def launch_missle(self):
         if self.reload_timer == 0:
             shot = Missle(self.x, self.y - 55)
             self.reload_timer = 50
             games.screen.add(shot)
-            print(Missle.speed)
 
-    def handle_hit(self):
-        return
+    def handle_collision(self, asteroid_strength):
+        self.defence -= asteroid_strength
+        if self.defence <= 0:
+            self.destroy()
+            print("Game Over!")
+        return True
 
     @staticmethod
-    def receive_upgrade():
-        Missle.speed -= 1
-        Missle.image = games.load_image("missle2.png")
+    def handle_missle_hit(*missle_power):
+        return False
+
+    @staticmethod
+    def receive_upgrade(upgrade_value):
+        Missle.speed -= upgrade_value
+        return True
 
 
 class Missle(games.Sprite):
     image = games.load_image("missle1.png")
     speed = -5
+    missle_power = 20
+    sound = games.load_sound("ast_crash1.wav")
 
     def __init__(self, x, y):
         super(Missle, self).__init__(image=Missle.image,
@@ -170,11 +206,17 @@ class Missle(games.Sprite):
 
     def check_hit(self):
         for space_object in self.overlapping_sprites:
-            space_object.handle_hit()
-            self.destroy()
+            object_was_hit = space_object.handle_missle_hit(self.missle_power)
+            if object_was_hit:
+                GameObject.score.value += 10
+                self.sound.play()
+                self.destroy()
 
-    def receive_upgrade(self):
-        return
+    def receive_upgrade(self, *upgrade_value):
+        return False
+
+    def handle_collision(self, strength_value):
+        return False
 
 
 def main():
